05aa8d6b907eaa5121d6c64860127ff51ce313ca
omeruren <omer.uren19@gmail.com>
2025-09-27 10:42:11 +0300

    IRepository

---
+++ Core/RensBlog.Application/Contracts/Persistance/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using RensBlog.Domain.Entities.Common;
+using System.Linq.Expressions;
+
+namespace RensBlog.Application.Contracts.Persistance;
+
+public interface IRepository<TEntity> where TEntity : BaseEntity
+{
+    Task<List<TEntity>> GetAllAsync();
+    Task<IQueryable> GetQueryAsync();
+    Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> filter);
+    Task<TEntity> GetByIdAsync(Guid id);
+    Task CrateAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TEntity entity);
+
+}
