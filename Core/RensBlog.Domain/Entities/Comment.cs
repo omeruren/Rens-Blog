49b8eadee523c9c70dad6dd9299b4f99e436efc7
omeruren <omer.uren19@gmail.com>
2025-09-28 14:50:28 +0300

    Comment and SubComment Entity

---
+++ Core/RensBlog.Domain/Entities/Comment.cs
@@ -0,0 +1,12 @@
+﻿using RensBlog.Domain.Entities.Common;
+
+namespace RensBlog.Domain.Entities;
+
+public class Comment : BaseEntity
+{
+    public string UserId { get; set; }
+    public string Body { get; set; }
+    public DateTime CommentDate { get; set; }
+
+    public IList<SubComment> SubComments { get; set; }
+}
---
+++ Core/RensBlog.Domain/Entities/SubComment.cs
@@ -0,0 +1,13 @@
+﻿using RensBlog.Domain.Entities.Common;
+
+namespace RensBlog.Domain.Entities;
+
+public class SubComment 
+{
+    public string UserId { get; set; }
+    public string Body { get; set; }
+    public DateTime CommentDate { get; set; }
+
+    public Guid CommentId { get; set; }
+    public Comment Comment { get; set; }
+}
