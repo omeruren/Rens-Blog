a577585bc4fcad28a86c399f19babcb9ba7d75b1
omeruren <omer.uren19@gmail.com>
2025-09-30 15:31:05 +0300

    Token Option Configurations

--- Core/RensBlog.Application/Extensions/ServiceRegistrations.cs
+++ Core/RensBlog.Application/Extensions/ServiceRegistrations.cs
@@ -1,12 +1,14 @@
 ﻿using FluentValidation;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RensBlog.Application.Behaviors;
+using RensBlog.Application.Options;
 using System.Reflection;
 
 namespace RensBlog.Application.Extensions;
 public static class ServiceRegistrations
 {
-    public static void AddApplication(this IServiceCollection services)
+    public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
         services.AddMediatR(config =>
@@ -16,5 +18,7 @@ public static class ServiceRegistrations
         });
 
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.Configure<JwtTokenOptions>(configuration.GetSection(nameof(JwtTokenOptions)));
     }
 }
