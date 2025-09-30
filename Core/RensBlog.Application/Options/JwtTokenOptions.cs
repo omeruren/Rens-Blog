2b8b23cd9cf923ad92631169c715ae26ff2477ae
omeruren <omer.uren19@gmail.com>
2025-09-30 15:27:48 +0300

    Jwt Token Options

---
+++ Core/RensBlog.Application/Options/JwtTokenOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace RensBlog.Application.Options;
+
+public class JwtTokenOptions
+{
+    public string Issuer { get; set; } // api.rensblog.com
+    public string Audience { get; set; } // www.rensblog.com
+    public string Key { get; set; }
+    public int ExpireInMunites { get; set; }
+
+}
